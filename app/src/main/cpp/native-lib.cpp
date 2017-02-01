d65b259dc32f895de11ba1290f2331c210ccd790
RogerZhang <roger.zhang.eng@gmail.com>
2017-02-01 23:07:18 +1100

    update greeting words.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -6,6 +6,6 @@ jstring
 Java_com_example_rogerzhang_rgtest_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from Roger Zhang Android Test";
     return env->NewStringUTF(hello.c_str());
 }
